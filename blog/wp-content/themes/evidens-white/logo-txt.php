35d2d29ec27fd66e81c31816f4e6cf65ae90c229
liangch <geshe@263.net>
2010-12-14 02:30:11 -0700

    Revert "resync"
    This reverts commit f012550c94171dedcaa82a3b5bcc3e1ab98f43a8.

--- blog/wp-content/themes/evidens-white/logo-txt.php
+++
@@ -1,4 +0,0 @@
-<div class="txt">
- <h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
- <p class="desc"><?php bloginfo('description'); ?></p>
-</div>
--- zaijiame/t/api/config-example.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('OAUTH_KEY','');
-define('OAUTH_SECRET','');
-define('BASE_URL','');
-define('DEBUG',FALSE);
-define('COMPRESS',FALSE);
-?>
